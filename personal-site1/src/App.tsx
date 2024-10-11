614ecf17787ae00741a0048d019b7c6c18e9745e
lauren-digsfact <lauren@digsfact.com>
2024-10-10 21:34:31 -0400

    uploaded projects

--- personal-site1/src/App.tsx
+++ personal-site1/src/App.tsx
@@ -1,14 +1,16 @@
 
 import './App.css' 
 import About from "./components/about";  
-import Navbar from "./components/navbar";
+import Navbar from "./components/navbar"; 
+import Project from "./components/project"; 
 
 function App() {
 
   return (
     <> 
       <Navbar />
-      <About /> 
+      <About />  
+      <Project /> 
     </>
   )
 }
--- personal-site1/src/main.tsx
+++ personal-site1/src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './index.css' 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js'; 
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
